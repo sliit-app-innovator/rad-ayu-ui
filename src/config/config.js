45490b2ee3c4cc721e4d708915dc45690ec30116
Ravindra Warnasooriya <warnasooriya.ravindra@gmail.com>
2024-04-17 14:32:39 +0530

    changes for CICD

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    baseUrl: 'ec2-18-221-137-27.us-east-2.compute.amazonaws.com/api/ayu/service/v1/',
+    baseUrl: 'https://ec2-18-221-137-27.us-east-2.compute.amazonaws.com/api/ayu/service/v1/',
     // baseUrl: 'http://localhost:8080/ayu/service/v1/',
 }
 
