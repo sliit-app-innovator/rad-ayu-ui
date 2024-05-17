1314a1f8ba33e256a8e6ace84b5f9063503480cd
Ravindra Warnasooriya <warnasooriya.ravindra@gmail.com>
2024-05-17 16:32:06 +0530

    changes for configurations files

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
-    // baseUrl: 'https://3.134.58.170/api/ayu/service/v1/',
-    baseUrl: 'http://localhost:8080/ayu/service/v1/',
+    baseUrl: 'https://3.134.58.170/api/ayu/service/v1/',
+    // baseUrl: 'http://localhost:8080/ayu/service/v1/',
 }
 
 export default config;
